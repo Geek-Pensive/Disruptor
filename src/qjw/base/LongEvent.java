854856c1621c36de6144939e33821c20ab50dede
Demo_QLife <qiaojiangwei123@qq.com>
2017-07-09 12:15:23 +0800

    Disrupt
    多线程框架

---
+++ src/qjw/base/LongEvent.java
@@ -0,0 +1,19 @@
+package qjw.base;
+
+//http://ifeve.com/disruptor-getting-started/
+
+/**
+ * 定义一个数据类
+ *
+ * @author q丶帮主      Date:2017年5月10日
+ */
+public class LongEvent { 
+    private long value;
+    public long getValue() { 
+        return value; 
+    } 
+ 
+    public void setValue(long value) { 
+        this.value = value; 
+    } 
+} 
